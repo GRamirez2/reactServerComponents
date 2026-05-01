1cdffc6e3dc0d9a8fd649b20f3d02ab5a2c40b66
George Ramirez <george.ramirez2@gmail.com>
2026-05-01 14:46:00 -0500

    added log to see login error

--- src/app/login/route.ts
+++ src/app/login/route.ts
@@ -4,5 +4,13 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   const signInUrl = await getSignInUrl();
 
+    console.log('WorkOS auth debug', {
+    signInUrl,
+    renderExternalUrl: process.env.RENDER_EXTERNAL_URL,
+    nextPublicRedirectUri: process.env.NEXT_PUBLIC_WORKOS_REDIRECT_URI,
+    workosRedirectUri: process.env.WORKOS_REDIRECT_URI,
+    port: process.env.PORT,
+  })
+
   return NextResponse.redirect(signInUrl);
 }
